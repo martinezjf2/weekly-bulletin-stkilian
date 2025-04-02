4b8d4606c95ca5553d6bf6b0dd4ad0f8e5b1afd9
Jeffrey Martinez <martinezjf2@gmail.com>
2025-04-02 11:08:06 -0400

    Auto-update MJML & HTML files on 2025-04-02T15:08:06.419Z

--- generateEmail.js
+++ generateEmail.js
@@ -103,5 +103,3 @@ updateMjmlEmail();
 // Added a github automatic push as the background using pm2,
 // make sure to have ssh installed in github as a key, and clone as ssh for macbook
 // Would need to update every Monday instead of every tuesday and set pm2 up again to happen every Monday
-
-// Generated a new comment to see if working
